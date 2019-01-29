e60fe6e27dfa9c9e7491392dc3f8e38324f96847
yart <jarocki.art@gmail.com>
2019-01-29 10:39:31 +0300

    MainMenu added

--- src/components/Home/MainNavigation/MainNavigation.vue
+++ src/components/Home/MainNavigation/MainNavigation.vue
@@ -2,20 +2,25 @@
   <nav class="main-navigation">
     <div class="container main-navigation__container">
       <company-logo />
+      <main-menu />
     </div>
   </nav>
 </template>
 
 <script>
 import CompanyLogo from '@/components/Interface/CompanyLogo/CompanyLogo'
+import MainMenu from '@/components/Interface/MainMenu/MainMenu'
 export default {
   name: 'MainNavigation',
   components: {
-    CompanyLogo
+    CompanyLogo,
+    MainMenu
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="sass">
+.main-navigation
+  &__container
+    display: flex
 </style>
