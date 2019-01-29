f40ec95ca16bd3692eb5d0055161e391775e3ffe
yart <jarocki.art@gmail.com>
2019-01-29 10:41:13 +0300

    set of components changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="page home-page main" />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
