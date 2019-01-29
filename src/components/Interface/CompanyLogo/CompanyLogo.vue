77d9841bf7fc5012081c1ba69feb1cbc3bd6c7e3
yart <jarocki.art@gmail.com>
2019-01-29 09:14:18 +0300

    CompanyLogo created

---
+++ src/components/Interface/CompanyLogo/CompanyLogo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="company-logo">
+    <picture>
+      <source
+        media="(min-width:768px)"
+        srcset="./img/CompanyLogo.png"
+      >
+      <img
+        src="./img/CompanyLogo_small.png"
+        alt="Теплострой"
+      >
+    </picture>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompanyLogo'
+}
+</script>
+
+<style scoped lang="sass">
+</style>
