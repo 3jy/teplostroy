30b9cc38b90309ef6080e40ecd191cf2a05a5e9e
yart <jarocki.art@gmail.com>
2019-01-29 09:13:42 +0300

    MainNavigation created

---
+++ src/components/Home/MainNavigation/MainNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="main-navigation">
+    <div class="container main-navigation__container">
+      <company-logo />
+    </div>
+  </nav>
+</template>
+
+<script>
+import CompanyLogo from '@/components/Interface/CompanyLogo/CompanyLogo'
+export default {
+  name: 'MainNavigation',
+  components: {
+    CompanyLogo
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
