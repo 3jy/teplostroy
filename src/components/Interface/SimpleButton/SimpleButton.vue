f6cc14e840d45f653ab9296617a1b86a153c5090
yart <jarocki.art@gmail.com>
2019-01-30 15:46:22 +0300

    SimpleButton component added

---
+++ src/components/Interface/SimpleButton/SimpleButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="btn">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'SimpleButton'
+}
+</script>
+
+<style lang="sass">
+.btn
+  font-size: 1em
+  color: #ffffff
+  background-color: $clr-main_btn
+  border: none
+  box-shadow: 0 0 10px 5px rgba($clr-main_btn, .3)
+  cursor: pointer
+  &:focus,
+  &:active
+    box-shadow: 0 0 10px 4px rgba($clr-main_btn, .6)
+  &_small
+    padding: .5em
+</style>
