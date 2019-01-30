fcd05754273fd0f38ef8c8fca5ecb030c75ee36c
yart <jarocki.art@gmail.com>
2019-01-30 15:47:55 +0300

    images path changed

--- src/components/Interface/CompanyLogo/CompanyLogo.vue
+++ src/components/Interface/CompanyLogo/CompanyLogo.vue
@@ -3,21 +3,12 @@
     <picture>
       <source
         media="(min-width:768px)"
-        srcset="./img/CompanyLogo.png"
+        srcset="./CompanyLogo.png"
       >
       <img
-        src="./img/CompanyLogo_small.png"
+        src="./CompanyLogo_small.png"
         alt="Теплострой"
       >
     </picture>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'CompanyLogo'
-}
-</script>
-
-<style scoped lang="sass">
-</style>
