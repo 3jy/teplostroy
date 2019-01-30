cbeb75646df88f6de0ad7501c9b81e64a01ee525
yart <jarocki.art@gmail.com>
2019-01-30 15:48:23 +0300

    FeedbackButton added

--- src/components/Home/MainNavigation/MainNavigation.vue
+++ src/components/Home/MainNavigation/MainNavigation.vue
@@ -3,6 +3,7 @@
     <div class="container main-navigation__container">
       <company-logo />
       <main-menu />
+      <feedback-button />
     </div>
   </nav>
 </template>
@@ -10,17 +11,21 @@
 <script>
 import CompanyLogo from '@/components/Interface/CompanyLogo/CompanyLogo'
 import MainMenu from '@/components/Interface/MainMenu/MainMenu'
+import FeedbackButton from '@/components/Interface/FeedbackButton/FeedbackButton'
 export default {
   name: 'MainNavigation',
   components: {
     CompanyLogo,
-    MainMenu
+    MainMenu,
+    FeedbackButton
   }
 }
 </script>
 
 <style scoped lang="sass">
 .main-navigation
+  width: 100vw
+  overflow-x: hidden
   &__container
     display: flex
 </style>
